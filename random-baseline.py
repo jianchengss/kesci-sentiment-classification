82258440a2fa8436485b2f84245a460080cf5f73
Jason Wang <jianchengss@163.com>
2019-03-08 00:18:09 +0800

    添加随机结果baseline，提交格式正确

--- random-baseline.py
+++ random-baseline.py
@@ -7,34 +7,28 @@
 
 import pandas as pd
 import random
-
-path = "./data/test.csv"
-# df_result = pd.read_csv(path, escapechar=)
-# print(df_result.head())
 import codecs
 
 random.seed(1)
-fr = codecs.open(path, 'rb', 'utf-8', 'ignore')
-df_result = pd.read_csv(fr, lineterminator='\n')
-print(df_result.head())
-print(df_result.shape)
+
+test_file_path = "./data/test.csv"
+result_path = "./submissions/random-baseline.csv"
+
+fr = codecs.open(test_file_path, 'rb', 'utf-8', 'ignore')
+test_df = pd.read_csv(fr, lineterminator='\n')
+print(test_df.head())
+print(test_df.shape)
+print(test_df.columns)
 data = []
-ids = df_result["ID"]
+ids = test_df["ID"]
+review = test_df['review']
+# for id , text in zip(ids, review):
+#     print("{} {}".format(id,text))
 for id in ids:
-    try:
-        float(id)
-    except:
-        continue
     r = random.random()
-    # print("%s,    %s" % (str(id), str(r)))
+    # print("%d,    %f" % (id, r))
     data.append([id, r])
 result = pd.DataFrame(data, columns=['ID', "Pred"])
 print(result.head())
-result.to_csv("./data/random-baseline.csv", index=0)
-print(df_result.shape)
-result_ids = list(result["ID"])
-
-print("=======")
-for i in result_ids:
-    if i not in list(ids):
-        print(i)
+print(result.shape)
+result.to_csv(result_path, index=0)
